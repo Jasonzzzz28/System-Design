dba178e5b9b91ab6346eea4aacf34a9ccc09c2ca
Siyuan Zhang <35844716+Jasonzzzz28@users.noreply.github.com>
2025-12-02 17:39:13 -0500

    Use HashRouter to resolve Github Pages showing 404 on refresh
    This is a quick hack for Github Pages. Revert if this site is moved away from Github Pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import NotesList from './components/NotesList';
 import NoteDetail from './pages/NoteDetail';
 
 function App() {
   return (
-    <BrowserRouter basename="/System-Design">
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<NotesList />} />
         <Route path="/notes/:slug" element={<NoteDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
